afdf58cc46e2725cbcdb1bc771fb783639463592
Holopotyliuk <golopotiluka@gmail.com>
2022-02-22 14:59:40 +0200

    update map, filter,reduce

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let lodashFilter = require('lodash/fp/filter');
 let lodashMap = require('lodash/fp/map')
 let lodashSum = require('lodash/fp/sum')
 let lodashMax = require('lodash/fp/max');
+const { F } = require('lodash/fp');
 let rectangles = [
     {
         width: 10,
@@ -20,9 +21,14 @@ let rectangles = [
         color: 'black'
     },
     {
-        width: 10,
-        height: 10,
+        width: 9,
+        height: 9,
         color: 'red'
+    },
+    {
+        width: 9,
+        height: 9,
+        color: 'black'
     },
     {
         width: 11,
@@ -31,14 +37,26 @@ let rectangles = [
     },
     {
         width: 11,
-        height: 10,
+        height: 11,
         color: 'red'
     }
 ]
+let small = r => r.width < 10;
 let hasColor = c => r => r.color == c;
 let isSquare = r => r.width == r.height;
 let calcArea = r => r.width * r.height;
 let calcPerimeter = r => (r.width + r.height) * 2;
+
+let selectSquare = fn => rectangles => rectangles.filter(fn)
+console.log('filter', selectSquare(isSquare)(rectangles))
+
+let calcAreas = fn => rectangles => rectangles.map(fn)
+console.log('map', calcAreas(calcArea)(rectangles))
+
+let sumCalcArea = fn => rectangles => rectangles.reduce((sum, r) => {
+    return sum + fn(r);
+}, 0)
+console.log('reduce', sumCalcArea(calcArea)(rectangles))
 
 let maxAreaWithBlackSquare = flow(
     lodashFilter(hasColor('black')),
@@ -46,52 +64,11 @@ let maxAreaWithBlackSquare = flow(
     lodashMap(calcArea),
     lodashMax
 )
-console.log(maxAreaWithBlackSquare(rectangles))
+console.log('max area', maxAreaWithBlackSquare(rectangles))
 
 let sumPerimeterWithRedRectangle = flow(
     lodashFilter(hasColor('red')),
     lodashMap(calcPerimeter),
     lodashSum
 )
-console.log(sumPerimeterWithRedRectangle(rectangles))
-
-function selectSquare(rectangles) {
-    return filter(isSquare, rectangles);
-}
-
-function filter(fn, list) {
-    let result = [];
-    for (let item of list)
-        if (fn(item))
-            result.push(item);
-    return result;
-}
-console.log('selectSquare', selectSquare(rectangles))
-
-function calcAreas(rectangles) {
-    return map(calcArea, rectangles);
-}
-
-function map(fn, list) {
-    let result = [];
-    for (let item of list)
-        result.push(fn(item));
-    return result
-}
-console.log('calcAreas', calcAreas(rectangles))
-let number = [1, 1, 1, 1, 1];
-function sum(number) {
-    return reduce(add, 0, number);
-}
-function add(a, b) {
-    return a + b;
-}
-
-function reduce(fn, initValue, list) {
-    let result = initValue;
-    for (let item of list)
-        result = fn(result, item)
-    return result;
-}
-console.log('sum', sum(number))
-
+console.log('sum perimeter', sumPerimeterWithRedRectangle(rectangles))
